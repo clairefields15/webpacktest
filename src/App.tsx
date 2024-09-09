d1e6f078d5d73dc75c49535b1e09eb50c32a3de3
Claire Fields <cfields@abovethetreeline.com>
2024-09-09 17:15:54 -0600

    render primary and secondary button

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import { Button, Divider, IconButton } from "@mui/material";
 export const App = () => (
     <div>
         <h1>Hello!</h1>
-        <Button>Button!</Button>
+        <Button color="primary">Primary Button!</Button>
+        <Button color="secondary">Secondary Button!</Button>
         <Divider />
         <IconButton>
             <PartyMode />
